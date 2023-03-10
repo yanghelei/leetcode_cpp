e2cbbe63635397946160e015f2ef26c1ed4bcef2
heyray <helei_yang@zju.edu.cn>
2023-03-10 13:13:01 +0800

    update.

--- 各类排序算法/快速排序.cpp
+++ 各类排序算法/快速排序.cpp
@@ -3,20 +3,18 @@
  * @Email: helei_yang@zju.edu.cn
  * @Date: 2023-03-09 01:07:44
  * @LastEditors: Heyray.Yang
- * @LastEditTime: 2023-03-09 01:34:25
+ * @LastEditTime: 2023-03-10 13:02:26
  * @FilePath: /LeetCode/各类排序算法/快速排序.cpp
  * @Description:
  */
 class Solution {
   void quickSort(vector<int>& nums, int startIndex, int endIndex) {
     if (startIndex >= endIndex) return;
-    // 基于随机的原则
-    int x = rand() % (endIndex - startIndex + 1) + startIndex;
-    swap(nums[startIndex], nums[x]);
 
     int firstNum = nums[startIndex];
 
-    int l = startIndex int r = endIndex;
+    int l = startIndex;
+    int r = endIndex;
     while (l < r) {
       // 从后往前走，将比第一个小的移到前面
       while (l < r && nums[r] >= firstNum) --r;
